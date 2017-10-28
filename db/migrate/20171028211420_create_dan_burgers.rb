213ee8462db4e01e9f8a1f695f026c6867ea925e
Nicolas Loewen <nic_loewen@yahoo.ca>
2017-10-28 21:26:45 +0000

    Added the faker gem, and then seeded the database

--- db/migrate/20171028211420_create_dan_burgers.rb
+++ db/migrate/20171028211420_create_dan_burgers.rb
@@ -2,10 +2,7 @@ class CreateDanBurgers < ActiveRecord::Migration[5.1]
   def change
     create_table :dan_burgers do |t|
       t.string :name
-      t.string :string
       t.string :address
-      t.string :string
-
       t.timestamps
     end
   end
