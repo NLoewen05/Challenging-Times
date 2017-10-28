7b9104febc35ba06a5f46fa5f4f0752a81afa71d
Nicolas Loewen <nic_loewen@yahoo.ca>
2017-10-28 18:50:10 +0000

    Part Two - Added all of the lizard functions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,12 @@
 class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+
+  def lizards
+    @lizards = params[:number]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   get 'about_us' , to: 'pages#about', as: 'about'
 
+  get 'lizards/:number', to: 'pages#lizards', as: 'lizards'
+
 end
