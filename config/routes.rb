c46c350e0375c77f6e95abfb4d6d7b3bf082a09b
Nicolas Loewen <nic_loewen@yahoo.ca>
2017-10-28 21:39:56 +0000

    I'm partially partial to your partial complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pages
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :pages
 
-  root to: 'pages#home', as: 'home'
+    root to: 'pages#home', as: 'home'
 
-  get 'about_us' , to: 'pages#about', as: 'about'
+    get 'about_us' , to: 'pages#about', as: 'about'
 
-  get 'lizards/:number', to: 'pages#lizards', as: 'lizards'
+    get 'lizards/:number', to: 'pages#lizards', as: 'lizards'
+
+    get 'danburgers', to: 'danburgers#view', as: 'danburgers'
+
+    get 'danburgers/:id', to: 'danburgers#single_view', as: 'burger'
 
 end
